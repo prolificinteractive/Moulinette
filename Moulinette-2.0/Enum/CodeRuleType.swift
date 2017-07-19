287c5807625f2cb0383ff5f6b85b4932ddbf0eac
Jonathan Samudio <jonathan@prolificinteractive.com>
2017-07-19 18:24:15 -0400

    Updated the rule collection

--- Moulinette-2.0/Enum/CodeRuleType.swift
+++ Moulinette-2.0/Enum/CodeRuleType.swift
@@ -12,7 +12,23 @@ struct CodeConventionRuleCollection: RuleCollection {
     
     func rules(projectData: ProjectData) -> [SwiftRule] {
         return [
+                AppDelegateSwiftRule(projectData: projectData),
+                SemiColonSwiftRule(projectData: projectData),
+                FinalClassesSwiftRule(projectData: projectData),
+                WeakIBOutletSwiftRule(projectData: projectData),
+                SinglePublicInternalSwiftRule(projectData: projectData),
+                ForceUnwrapSwiftRule(projectData: projectData),
+                MarkUsageSwiftRule(projectData: projectData),
+                SingleEnumCaseSwiftRule(projectData: projectData),
+                InternalModifierSwiftRule(projectData: projectData),
+                TypeInferenceSwiftRule(projectData: projectData),
+                FontEncapsulationSwiftRule(projectData: projectData),
+                ColorEncapsulationSwiftRule(projectData: projectData),
                 CompletionWeakSwiftRule(projectData: projectData),
+                ToDoStorySwiftRule(projectData: projectData),
+                ToDoCountSwiftRule(projectData: projectData),
+                UsesLocalizationSwiftRule(projectData: projectData),
+                LocalizedStringSwiftRule(projectData: projectData)
         ]
     }
 }
