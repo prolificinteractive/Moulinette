427568ee258ba3758e30a8b177f22da03e6d7db1
Jonathan Samudio <jonathan2457@gmail.com>
2017-08-22 14:59:44 -0400

    Added tests for the single internal type.

--- Moulinette-2.0-Tests/XCTestExtension.swift
+++ Moulinette-2.0-Tests/XCTestExtension.swift
@@ -10,13 +10,21 @@ import XCTest
 
 extension XCTest {
     
+    /// Generates a project data with the given parameters.
+    ///
+    /// - Parameter components: Project components to add to the ApplicationComponents.
+    /// - Returns: Initialized project data.
+    func projectData(components: ProjectComponents) -> ProjectData {
+        let components = ApplicationComponents(with: components)
+        return ProjectData(applicationComponents: components)
+    }
+    
     /// Returns project data for the given parameters.
     ///
     /// - Parameter line: Single line of the project file.
     /// - Returns: Project data with a single file with a single line.
     func projectData(line: String) -> ProjectData {
-        let applicationComponents = ApplicationComponents(with: ["Sample": [line]])
-        return ProjectData(applicationComponents: applicationComponents)
+        return projectData(components: ["Sample": [line]])
     }
     
     /// Returns project data for the given parameters.
@@ -25,15 +33,20 @@ extension XCTest {
     /// - Parameter fileName: Name of the file
     /// - Returns: Project data with a single file with a single line.
     func projectData(fileName: String, line: String) -> ProjectData {
-        let applicationComponents = ApplicationComponents(with: [fileName: [line]])
-        return ProjectData(applicationComponents: applicationComponents)
+        return projectData(components: [fileName: [line]])
     }
     
     /// Empty project data.
     ///
     /// - Returns: Empty project data.
     func emptyProjectData() -> ProjectData {
-        let components = ApplicationComponents(with: [:])
-        return ProjectData(applicationComponents: components)
+        return projectData(components: [:])
+    }
+    
+    /// Generates a project data object.
+    ///
+    /// - Returns: Project data with an empty single file.
+    func emptyProjectFile() -> ProjectData {
+        return projectData(components: ["Sample": []])
     }
 }
