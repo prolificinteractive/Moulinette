5409ed8b2c45a8658cba5b094ea6db9bfd850463
Jonathan Samudio <jonathan2457@gmail.com>
2017-09-22 15:38:57 -0400

    Added tests for type inference rule.

---
+++ Moulinette-2.0-Tests/Rules/TypeInferenceSwiftRuleTests.swift
@@ -0,0 +1,65 @@
+//
+//  TypeInferenceSwiftRuleTests.swift
+//  Moulinette-2.0
+//
+//  Created by Jonathan Samudio on 9/22/17.
+//  Copyright © 2017 Prolific Interactive. All rights reserved.
+//
+
+import XCTest
+
+@testable import Moulinette_2_0
+class TypeInferenceSwiftRuleTests: XCTestCase {
+
+    var sut: TypeInferenceSwiftRule!
+    
+    func test_RunCorrectTypeInitialize() {
+        sut = TypeInferenceSwiftRule(projectData: projectData(line: "var sample = Example()"))
+        
+        let grade = sut.run()
+        
+        XCTAssertEqual(grade.violations, 0)
+    }
+    
+    func test_RunUnnecessaryTypeInitialize() {
+        sut = TypeInferenceSwiftRule(projectData: projectData(line: "var sample: Example = Example()"))
+        
+        let grade = sut.run()
+        
+        XCTAssertEqual(grade.violations, 1)
+    }
+    
+    func test_RunCorrectTypeStringInitialize() {
+        sut = TypeInferenceSwiftRule(projectData: projectData(line: "var sample = \"\""))
+        
+        let grade = sut.run()
+        
+        XCTAssertEqual(grade.violations, 0)
+    }
+    
+    func test_RunUnnecessaryTypeStringInitialize() {
+        sut = TypeInferenceSwiftRule(projectData: projectData(line: "var sample: String = \"\""))
+        
+        let grade = sut.run()
+        
+        XCTAssertEqual(grade.violations, 1)
+    }
+    
+// MARK: - Default Tests
+    
+    func testRun_EmptyProject() {
+        sut = TypeInferenceSwiftRule(projectData: emptyProjectData())
+        
+        let grade = sut.run()
+        
+        XCTAssertEqual(grade.violations, 0)
+    }
+    
+    func testRun_EmptySingleFile() {
+        sut = TypeInferenceSwiftRule(projectData: emptyProjectFile())
+        
+        let grade = sut.run()
+        
+        XCTAssertEqual(grade.violations, 0)
+    }
+}
